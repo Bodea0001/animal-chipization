c08df46352a8ecf3c428b1cb4ece66a3748cf316
Bogdan Objilean <bodea0001@gmail.com>
2023-03-02 21:47:12 +0300

    add setup

---
+++ Sources/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="animal-chipization",
+    version="1.0.0",
+    url="https://github.com/Bodea0001/animal-chipization",
+    packages=find_packages()
+)
