2769d95276efc1bed1a2445bba666532c8647c6c
Bogdan Objilean <bodea0001@gmail.com>
2023-03-02 21:38:07 +0300

    small fix

--- Sources/db/database.py
+++ Sources/db/database.py
@@ -1,9 +1,8 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from config.config import POSTGRESQL_CONFIG
 
-POSTGRESQL_CONFIG = os.environ["POSTGRESQL_CONFIG"]
 
 engine = create_engine(POSTGRESQL_CONFIG, echo=True, future=True)
 
