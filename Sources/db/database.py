c1a077f7b3010ca42c30f937f5ba0f475bdf238f
Bogdan Objilean <bodea0001@gmail.com>
2023-03-07 11:42:39 +0300

    turn off echo

--- Sources/db/database.py
+++ Sources/db/database.py
@@ -4,6 +4,6 @@ from sqlalchemy.orm import sessionmaker
 from config.config import POSTGRESQL_CONFIG
 
 
-engine = create_engine(POSTGRESQL_CONFIG, echo=True, future=True)
+engine = create_engine(POSTGRESQL_CONFIG, echo=False, future=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
