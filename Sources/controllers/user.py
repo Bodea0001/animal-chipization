9af2c52feff4bf8efff8dc7037e7d36c15b57db8
Bogdan Objilean <bodea0001@gmail.com>
2023-04-01 14:06:10 +0300

    add role checker

--- Sources/controllers/user.py
+++ Sources/controllers/user.py
@@ -1,7 +1,9 @@
+from typing import Literal
 from sqlalchemy.orm import Session
 from fastapi.security import HTTPBasicCredentials
 from fastapi import Depends, HTTPException, status
 
+from db import models
 from models import schemas
 from db.crud import get_user
 from controllers.db import get_db
@@ -9,6 +11,11 @@ from controllers.auth import security
 from controllers.password import verify_password
 from controllers.validation import validate_account
 
+
+def check_role(user: schemas.Account, allowed_roles: list[schemas.Role]):
+    if user.role not in allowed_roles:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    
 
 async def get_current_account(
     db: Session = Depends(get_db),
